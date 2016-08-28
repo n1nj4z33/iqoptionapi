e9fcc4de582a42261e96468b3a4ca0690a27433d
Alexander Skiridomov <Alexander Skiridomov>
2016-08-28 15:39:59 +0300

    + fix docs;

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import os
+import sys
+
+sys.path.append(os.path.join('../iqapi'))
+
 extensions = ["sphinx.ext.todo",
               "sphinx.ext.viewcode",
               "sphinx.ext.autodoc"]
+
 master_doc = "index"
