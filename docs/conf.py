7566d4ef700e431c7cea8eeee4ac9e7fca677418
Alexander Skiridomov <n1nj4z33@gmail.com>
2016-08-31 18:58:03 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../iqoption_api"))
+sys.path.insert(0, os.path.abspath("."))
 
 extensions = ["sphinx.ext.todo",
               "sphinx.ext.viewcode",
