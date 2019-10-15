8aee678de2ff085384fad17cf4cd3724f1eed48b
lu-yi-hsun <yihsun1992@gmail.com>
2019-10-15 22:48:11 +0800

    check

--- tests/test_Login.py
+++ tests/test_Login.py
@@ -2,15 +2,13 @@ import unittest
  
 import os
 from iqoptionapi.stable_api import IQ_Option
+import logging
+logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(message)s')
 
 email=os.getenv("email")
 password=os.getenv("password")
-class TestLogin(unittest.TestCase):
-  
-    def test_login(self):
-        I_want_money=IQ_Option(email,password)
-        self.assertEqual(I_want_money.check_connect(), True)
-         
  
-if __name__ == '__main__':
-    unittest.main()
+ 
+I_want_money=IQ_Option(email,password)
+ 
+ 
