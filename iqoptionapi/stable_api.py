4c5ae14895e6144e20e7d3c883d0198be368c26a
lu-yi-hsun <yihsun1992@gmail.com>
2019-11-22 22:58:01 +0800

    5.1

--- iqoptionapi/stable_api.py
+++ iqoptionapi/stable_api.py
@@ -19,7 +19,7 @@ def nested_dict(n, type):
 
 
 class IQ_Option:
-    __version__ = "5.0"
+    __version__ = "5.1"
 
     def __init__(self, email, password):
         self.size = [1, 5, 10, 15, 30, 60, 120, 300, 600, 900, 1800,
