d83e54bc1c9848dca44ef290ccaa8b48ddaf43a0
zidokobik <caongocdavidlong@gmail.com>
2019-12-24 15:49:38 +0000

    fix buy and multi buy

--- iqoptionapi/ws/chanels/buyv2.py
+++ iqoptionapi/ws/chanels/buyv2.py
@@ -16,11 +16,13 @@ class Buyv2(Base):
         :param active: The buying active.
         :param direction: The buying direction.
         """
-        # thank Darth-Carrotpie's code 
-        #https://github.com/Lu-Yi-Hsun/iqoptionapi/issues/6
       
-        exp,idx=get_expiration_time(int(self.api.timesync.server_timestamp),duration)  
-
+        exp, idx = get_expiration_time(int(self.api.timesync.server_timestamp),duration)
+        
+        if idx < 5:
+            option = 3  # turbo
+        else:
+            option = 1  # non-turbo / binary
 
         data = {
             "name": "binary-options.open-option",
@@ -28,45 +30,14 @@ class Buyv2(Base):
             "body": {
                 "user_balance_id": int(self.api.profile.balance_id),
                 "active_id": active,
-                "option_type_id": 3,
+                "option_type_id": option,
                 "direction": direction.lower(),
                 "expired": int(exp),
                 "refund_value": 0,
                 "price": price,
-                "value": 0, #Preset to 0, dont worry won't affect
-                "profit_percent": 0 #Iqoption accept any value lower than the actual percent, dont worry it won't affect actual earning
+                "value": 0,  # Preset to 0, don't worry won't affect the actual buy contract
+                "profit_percent": 0  # IQOption accept any value lower than the actual percent, don't worry it won't affect actual earning
             }
         }
 
         self.send_websocket_request(self.name, data)
-
-    # thank Darth-Carrotpie's code 
-    #https://github.com/Lu-Yi-Hsun/iqoptionapi/issues/6
-"""    def get_expiration_time(self, duration):
-        exp=int(self.api.timesync.server_timestamp)
-        if duration>=1 and duration<=5:
-            option="turbo"
-            #Round to next full minute
-            #datetime.datetime.now().second>30
-            if (exp % 60) > 30:
-                exp = exp - (exp % 60) + 60*(duration+1)
-            else:
-                exp = exp - (exp % 60)+60*(duration)
-        elif duration > 5:
-            option = "binary"
-            period = int(round(duration / 15))
-            tmp_exp = exp - (exp % 60)#nuima sekundes
-            tmp_exp = tmp_exp - (tmp_exp%3600)#nuimam minutes
-            j=0
-            while exp > tmp_exp + (j)*15*60:#find quarter
-                j = j+1
-            if exp - tmp_exp > 5 * 60:
-                quarter = tmp_exp + (j)*15*60
-                exp = quarter + period*15*60
-            else:
-                quarter = tmp_exp + (j+1)*15*60
-                exp = quarter + period*15*60
-        else:
-            logging.error("ERROR get_expiration_time DO NOT LESS 1")
-            exit(1)
-        return exp, option"""
